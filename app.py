b101733540492506a8e7424a140e37988001e321
HadiCyb <hadihabibi2000@gmail.com>
2020-03-12 12:22:34 +0330

    weight converter improved

--- app.py
+++ app.py
@@ -1,15 +1,33 @@
 
-count = 0
-while count <= 4 :
-    weight = float(input('weight: '))
-    l_or_k = input('(L)bs or (K)g ? ')
+print('''guide:
+enter (l) to convert to kilograms 
+or enter (k) to convert to pounds 
+and 'quit' for exiting ,
+press empty enter to let the program get started again ...''')
 
-    if l_or_k.lower() == 'l':
-       weight *= 0.45
-       print(f'your weight {weight} kilograms')
-    elif l_or_k.lower() == 'k':
-        weight /=  0.45
+command=''
+while command == '' :
+    weight = float(input('weight: '))
+    l_or_k = input('(L)bs or (K)g ? ').lower()
+
+    if l_or_k == 'l':
+        weight *= 0.45
+        print(f'your weight {weight} kilograms')
+
+    elif l_or_k == 'k':
+        weight /= 0.45
         print(f'your weight {weight} pounds')
+
     else:
         print('something were wrong...')
+
+    command = input('> ').lower()
+    if command == 'quit':
+        break
+    elif command != '':
+        i=1
+        while i<=3 and command != '':
+            print(' its wrong input !!')
+            command = input('> ').lower()
+            i+=1
 
