6bfab6436bc531e0ceeb9277703d74e700be98fb
HadiCyb <hadihabibi2000@gmail.com>
2020-03-27 18:22:34 +0430

    inheritence from classes

--- app5.py
+++ app5.py
@@ -3,6 +3,14 @@ try:
         def __init__(self, age):
             self.age=age
 
+    class Teenage(Person):
+        def programmer(self):
+             print('junior developer')
+             return ''
+
+    class Older(Person):
+         pass
+
 
     print('''
              to fine out age of each person,
@@ -13,10 +21,15 @@ try:
     name = input('name: ').lower()
 
     john = Person(25)
-    hadi = Person(21)
-    mosh = Person(32)
+    hadi = Teenage(21)
+    mosh = Older(32)
 
-    print(vars()[name].age)
+    age = vars()[name].age
+    print(f'the age is: {age} years old')
+
+    if name == 'hadi':
+
+        print(hadi.programmer())
 
 except KeyError :
     print('input name with letters only !')
