af2d2a404536f130919c3fbb4d5351092b43cbca
HadiCyb <hadihabibi2000@gmail.com>
2020-04-04 19:48:18 +0430

    modules and packaging

--- app3.py
+++
@@ -1,8 +0,0 @@
-#removin duplicate number in an array
-list = [1, 4, 6, 8, 8, 5, 1, 8]
-for num in list :
-  counter = list.count(num)
-  if counter > 1:
-        place = list.index(num)
-        del list[place]
-print(list)
---
+++ ecommerce/hadi.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+class Training:
+    path = Path()
+    for file in path.glob('*'):
+        print(file)
