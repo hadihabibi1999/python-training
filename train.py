2ff704024883300a8b3bd710f59855fe1804ae6f
HadiCyb <hadihabibi2000@gmail.com>
2020-04-17 19:39:21 +0430

    make a tupple by a loop

---
+++ train.py
@@ -0,0 +1,7 @@
+list = ['hadi', 'mosh', 'peyman', 'john']
+
+
+for name, index in enumerate(list):
+#same as : for item in list:
+#            name, index = item  -> item is a tupple 
+    print(name,index)
